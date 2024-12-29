0a4cc799869813a1123517c58be93611c8becba2
Zhikai Tang <zhikai.tang123@gmail.com>
2024-12-28 19:39:22 -0600

    adfadf

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/number-ranking/'  // Your repo name
+  base: '/'  // Changed from '/number-ranking/' to '/'
 })
